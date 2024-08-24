0aeea956c9204b5ef81923bfdf8fedf87b124364
jayraphoriginal <rafel101990@gmail.com>
2024-08-24 09:08:26 +0000

    update bbm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
     </head>
 
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        {{-- <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
@@ -128,6 +128,8 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
+
+        <h1> Under Maintenance</h1>
     </body>
 </html>
